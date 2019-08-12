2841638cb55eb1a20c327b4022fa119ca0393de3
Sufian Rashidi <Sufian100100@gmail.com>
2019-08-12 16:37:27 -0400

    Developer 2

--- src/com/Demo.java
+++ src/com/Demo.java
@@ -108,8 +108,8 @@ public class Demo {
 		
 		//System.out.print(test());
 		//System.out.print(incoming);
-		System.out.println("developer 2");
 
+		
 		List<Data> list = showAll(teams);
 		//return showAll(incoming);
 		return list;
